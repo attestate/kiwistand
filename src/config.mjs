eda1efac9f9e501ffe186627b2aa18d291010631
Tim Daubenschütz <tim@daubenschuetz.de>
2021-12-21 19:40:40 +0100

    Make bind address configurable

--- src/config.mjs
+++ src/config.mjs
@@ -1,6 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { env } from "process";
+
+const { BIND_ADDRESS_V4 } = env;
+
 const config = {
   addresses: {
-    listen: ["/ip4/127.0.0.1/tcp/0"]
+    listen: [`/ip4/${BIND_ADDRESS_V4}/tcp/0`]
   }
 };
 export default config;
--- test/test_dotenv.mjs
+++ test/test_dotenv.mjs
@@ -1,19 +1,35 @@
 //@format
 import test from "ava";
-import { readFile } from "fs/promises";
+import { readFile, access } from "fs/promises";
+import { constants } from "fs";
 import { resolve } from "path";
 
 import { appdir } from "../src/utils.mjs";
 
 test("that repo contains a .env-copy file with all possible configuration options", async t => {
-  const name = ".env-copy";
-  const envPath = resolve(appdir(), name);
-  const content = (await readFile(envPath)).toString();
+  const copyName = ".env-copy";
+  const copyPath = resolve(appdir(), copyName);
+  const content = (await readFile(copyPath)).toString();
+  let assertions = 1;
   t.truthy(content);
+
+  const name = ".env";
+  const envPath = resolve(appdir(), name);
+
+  try {
+    await access(envPath, constants.F_OK);
+    const envContent = (await readFile(envPath)).toString();
+    const expr = new RegExp(".*=.*", "gm");
+    const envMatches = envContent.match(expr);
+    const copyMatches = content.match(expr);
+    t.is(envMatches.length, copyMatches.length);
+    assertions += 1;
+  } catch {}
 
   const options = ["LOG_LEVEL", "NODE_ENV"];
 
-  t.plan(options.length + 1);
+  assertions += options.length;
+  t.plan(assertions);
   for (let option of options) {
     t.true(content.includes(option), `${option} missing`);
   }
