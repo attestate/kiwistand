4acd239895e2a4891320f3f1c07e528eaa052990
Tim Daubenschütz <tim@daubenschuetz.de>
2023-04-16 23:20:58 +0200

    Update index.mjs

--- src/views/index.mjs
+++ src/views/index.mjs
@@ -30,7 +30,7 @@ export default async function index(trie) {
         <meta charset="utf-8" />
         <meta name="referrer" content="origin" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon.png" />
         <link rel="stylesheet" type="text/css" href="news.css" />
         <link rel="shortcut icon" href="favicon.ico" />
         <title>Kiwi News</title>
