98b3ad54f259fdd4d17e424130f804c26c63e22b
Tim Daubenschütz <tim@daubenschuetz.de>
2023-05-01 22:15:38 +0200

    Integrate privacy policy

--- src/views/components/footer.mjs
+++ src/views/components/footer.mjs
@@ -5,32 +5,8 @@ import vhtml from "vhtml";
 
 const html = htm.bind(vhtml);
 const footer = html`
-  <div id="privacy-notice" style="width: 85%; padding: 5px; font-size: 10px;">
-    <h2 style="font-size: 12px; margin: 0 0 3px; color: #f0f0f0;">
-      Privacy Notice & Cookie Policy
-    </h2>
-    <p style="margin: 0 0 3px; color: #e0e0e0;">
-      We use Google Analytics, a web analysis service provided by Google Inc.,
-      on our website. Google Analytics uses cookies to analyze your use of the
-      website, generate reports on website activity, and provide other services
-      related to website usage and internet usage.
-    </p>
-    <p style="margin: 0 0 3px; color: #e0e0e0;">
-      Google may transfer this information to third parties if required by law
-      or if third parties process this data on behalf of Google. Google will not
-      associate your IP address with any other data held by Google.
-    </p>
-    <p style="margin: 0; color: #e0e0e0;">
-      By using this website, you consent to the processing of data about you by
-      Google in the manner and for the purposes set out above. For more
-      information, please review our
-      <a
-        href="/privacy-policy"
-        style="color: #ffffff; text-decoration: underline;"
-        >Privacy Policy</a
-      >.
-    </p>
-  </div>
+  <br />
+  <a href="/privacy-policy">Privacy Policy</a>
   <script src="bundle.js"></script>
 `;
 export default footer;
