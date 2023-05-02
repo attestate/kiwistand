514ca45e1ba15af9fe3159405577d1d2fb611c34
Tim Daubenschütz <tim@daubenschuetz.de>
2023-05-03 00:02:29 +0200

    Increase crawler db maxReaders
    - Fixes #34

--- src/chainstate/registry.mjs
+++ src/chainstate/registry.mjs
@@ -4,7 +4,12 @@ import { resolve } from "path";
 
 export async function allowlist() {
   const path = resolve(process.env.DATA_DIR, "call-block-logs-load");
-  const db = database.open(path);
+  // NOTE: On some cloud instances we ran into problems where LMDB reported
+  // MDB_READERS_FULL which exceeded the LMDB default value of 126. So we
+  // increased it and it fixed the issue. So we're passing this option in the
+  // @attestate/crawler.
+  const maxReaders = 500;
+  const db = database.open(path, maxReaders);
   const name = database.order("call-block-logs");
   const subdb = db.openDB(name);
   const all = await database.all(subdb, "");
