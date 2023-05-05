debb66ab676053a82b9aab789f68049a5c9a4528
Tim Daubenschütz <tim@daubenschuetz.de>
2023-05-05 12:25:09 +0200

    Add test if digest is canonical

--- test/id_test.mjs
+++ test/id_test.mjs
@@ -30,6 +30,28 @@ async function teardown(t) {
   await unlink(idPath);
 }
 
+test("if digest is canonical for various messages", (t) => {
+  const message0 = {
+    timestamp: "2023-01-15T16:06:31.749Z",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  };
+  const outcome0 = toDigest(message0);
+  t.is(
+    outcome0.digest,
+    "93aaa3b8de60ed329538dd86d9511afcf2f0c690f33da5ac2469c28848d1f0b6"
+  );
+
+  const message1 = {
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    timestamp: "2023-01-15T16:06:31.749Z",
+  };
+  const outcome1 = toDigest(message1);
+  t.is(
+    outcome1.digest,
+    "93aaa3b8de60ed329538dd86d9511afcf2f0c690f33da5ac2469c28848d1f0b6"
+  );
+});
+
 test("generating digest from message", (t) => {
   const message = {
     timestamp: "2023-01-15T16:06:31.749Z",
