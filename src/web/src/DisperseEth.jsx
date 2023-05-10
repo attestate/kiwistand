b42777ea362098cc78e37d1ab5d9df790c6fe22c
Tim Daubenschütz <tim@daubenschuetz.de>
2023-05-10 14:39:04 +0200

    Use wagmi

--- src/web/src/DisperseEth.jsx
+++ src/web/src/DisperseEth.jsx
@@ -1,9 +1,8 @@
 // @format
 import React, { useState } from 'react';
-import { useAccount, useEnsName, WagmiConfig } from "wagmi";
-import { ConnectKitProvider } from "connectkit";
-import { sendTransaction, prepareSendTransaction } from '@wagmi/core'
-import { Contract, utils, BigNumber } from 'ethers'
+import { usePrepareContractWrite, useContractWrite, useEnsName, WagmiConfig } from "wagmi";
+import { ConnectKitProvider, useConnectKit } from "connectkit";
+import { utils } from "ethers";
 
 import { EnsName } from "./EnsName.jsx";
 import client from "./client.mjs";
@@ -19,40 +18,19 @@ const Container = (props) => {
   );
 };
 
-const abi = [
-    {
-      name: 'disperseEther',
-      type: 'function',
-      stateMutability: 'payable',
-      inputs: [
-        {
-          name: 'recipients',
-          type: 'address[]',
-        },
-        {
-          name: 'values',
-          type: 'uint256[]',
-        },
-      ],
-      outputs: [],
-    },
-  ];
-
-
 const DisperseEth = (props) => {
-  const { isConnected } = useAccount();
-  // NOTE: takes a list of comma separated Ethereum addresses
+  const { connected } = useConnectKit();
   const addresses = props.addresses.split(",");
-  // NOTE: takes a list of comma separated upvote counts 200, 30, 20, ...
   const shares = props.shares.split(",");
 
   const zip = (arr1, arr2) => arr1.map((k, i) => [k, arr2[i]]);
   const pairs = zip(addresses, shares);
 
   const [checkedState, setCheckedState] = useState(
-    new Array(pairs.length).fill(true)
+    new Array(pairs.length).fill(false)
   );
 
+  // State for input
   const [inputValue, setInputValue] = useState(0);
 
   const handleChange = (position) => {
@@ -68,37 +46,47 @@ const DisperseEth = (props) => {
 
   const recipients = [];
   const values = [];
-  pairs.forEach((pair, i) => {
-    let input;
-    if (!isNaN(inputValue) && inputValue === "") {
-      input = utils.parseEther("0");
-    } else {
-      input = utils.parseEther(inputValue.toString());
-    }
 
+  pairs.forEach((pair, i) => {
     if (checkedState[i]) {
       recipients.push(pair[0]);
-      const share = BigNumber.from(pair[1]);
-      const total = BigNumber.from(totalPoints.toString());
-      const value = share.mul(input).div(total);
-      values.push(value);
+      values.push(Math.floor((pair[1] / totalPoints) * inputValue * 10**18));
     }
   });
 
-  const handleClick = async () => {
-    const addr = "0xD152f549545093347A162Dce210e7293f1452150";
-    const contract = new Contract(addr, abi);
-    const data = contract.interface.encodeFunctionData('disperseEther', [recipients, values]);
-    
-    const config = await prepareSendTransaction({
-      to: addr,
-      value: utils.parseEther(inputValue.toString()),
-      data,
-    });
-  
-    const { hash } = await sendTransaction(config);
-    console.log(hash);
-  };
+  let etherValue = "0";
+  if (inputValue !== "" && !isNaN(inputValue) && /^[0-9.]+$/.test(`${inputValue}`)) {
+    etherValue = utils.parseEther(`${inputValue}`).toString();
+    console.log(etherValue);
+  }
+
+  const { config } = usePrepareContractWrite({
+    address: '0xD152f549545093347A162Dce210e7293f1452150',
+    abi: [
+      {
+        name: 'disperseEther',
+        type: 'function',
+        stateMutability: 'payable',
+        inputs: [
+          {
+            name: 'recipients',
+            type: 'address[]',
+          },
+          {
+            name: 'values',
+            type: 'uint256[]',
+          },
+        ],
+        outputs: [],
+      },
+    ],
+    functionName: 'disperseEther',
+    args: [recipients, values],
+    enabled: Boolean(recipients.length && values.length),
+    value: etherValue
+  });
+
+  const { write } = useContractWrite(config);
   
   return (
     <>
@@ -107,10 +95,9 @@ const DisperseEth = (props) => {
         <a href="https://etherscan.io/address/0xD152f549545093347A162Dce210e7293f1452150#code" target="_blank" rel="noopener noreferrer">
           etherscan.io
         </a>, you can select the curators that you found useful and send them ETH!
-      </p>
-          {pairs.map((pair, i) => (
-        <div key={i} style={{ display: 'flex', alignItems: 'center', justifyContent: 'start', marginBottom: '10px' }}>
-          {isConnected && 
+      </p>      {pairs.map((pair, i) => (
+        <div key={i} style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '10px' }}>
+          {connected && 
             <input type="checkbox" id={`checkbox${i}`} defaultChecked={checkedState[i]} onChange={() => handleChange(i)} />
           }
           <label htmlFor={`checkbox${i}`} style={{minWidth: '200px', whiteSpace: 'nowrap'}}>
@@ -118,12 +105,17 @@ const DisperseEth = (props) => {
           </label>
         </div>
       ))}
-      {isConnected &&
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'start', marginTop: '20px' }}>
+      {connected &&
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: '20px' }}>
           <input type="number" value={inputValue} 
             onChange={e => setInputValue(e.target.value === '' ? '' : Number(e.target.value))}
             placeholder="1.0" />
-          <button onClick={handleClick}>Disperse</button>
+          <span style={{ paddingLeft: '10px' }}>Ether (use English decimal format, e.g., "1.0")</span>
+          <button onClick={(e) => {
+            e.preventDefault();
+            showMessage("Please sign the message in your wallet!");
+            write?.();
+          }} disabled={!write}>Disperse</button>
         </div>
       }
     </>
@@ -131,3 +123,4 @@ const DisperseEth = (props) => {
 };
 
 export default Container;
+
