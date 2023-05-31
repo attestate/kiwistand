3fb57459f75a877432b14d852f33159d1342f7e7
Tim Daubenschütz <tim@daubenschuetz.de>
2023-05-31 18:41:30 +0200

    Upgrade connectkit for Dawn Wallet support

--- src/web/src/client.mjs
+++ src/web/src/client.mjs
@@ -6,6 +6,8 @@ const client = createClient(
   getDefaultClient({
     appName: "Kiwi News",
     alchemyId: "3ZBBnBhNn0nMmNcdgXFpqWqC981hd1Z2",
+    // NOTE: https://github.com/wagmi-dev/wagmi/discussions/2240#discussioncomment-6051864
+    //walletConnectProjectId: "cd46d2fcf6d171fb7c017129868fa211",
   })
 );
 export default client;
