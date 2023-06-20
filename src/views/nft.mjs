9fb9b75d2bf462cae315a06f2b505c5ba3b1c504
MacB <mac@macbudkowski.com>
2023-06-20 14:05:51 +0200

    changed the text on nft page

--- src/views/nft.mjs
+++ src/views/nft.mjs
@@ -137,18 +137,6 @@ export default function (theme) {
                       </div>
                       <div class="text-container">
                       <p style="font-size: 0.9rem;">
-                        Kiwi News is the prime feed for engineers building a decentralized future.
-                      </p>
-                      <p style="font-size: 0.9rem;">
-                        You don’t need to scroll Twitter anymore - all our content is handpicked and curated by the crypto-savvy community. 
-                      </p>
-                      <p style="font-size: 0.9rem;">
-                        We all follow <u><a href="https://hackmd.io/a-r--DX2T5uEEKX0Z8PRlQ?view">Submission Guidelines</a></u> to protect the feed from mid and off-topic content.
-                      </p>
-                      <p style="font-size: 0.9rem;">
-                        We are also credibly neutral - Kiwi News is built on top of the <u><a href="https://github.com/attestate/kiwistand/">P2P network</a></u>.
-                      </p>
-                      <p style="font-size: 0.9rem;">
                         <b>If you want to join our community, mint the Kiwi NFT to:</b>
                       </p>
                       <ul class="list-unstyled selling-points">
