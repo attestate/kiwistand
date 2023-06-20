cc2e085281e70e8ba569737483565d9ed68e10ac
MacB <mac@macbudkowski.com>
2023-06-20 15:43:52 +0200

    linked the community

--- src/views/nft.mjs
+++ src/views/nft.mjs
@@ -118,7 +118,7 @@ export default function (theme) {
                         Kiwi News is the prime feed for engineers building a decentralized future.
                       </p>
                       <p style="font-size: 0.9rem;">
-                        You don’t need to scroll Twitter anymore - all our content is handpicked and curated by the crypto-savvy community. 
+                        You don’t need to scroll Twitter anymore - all our content is handpicked and curated by the crypto-savvy <a href="https://news.kiwistand.com/community">Community</a>. 
                       </p>
                       <p style="font-size: 0.9rem;">
                         We all follow <u><a href="https://hackmd.io/a-r--DX2T5uEEKX0Z8PRlQ?view">Submission Guidelines</a></u> to protect the feed from mid and off-topic content.
