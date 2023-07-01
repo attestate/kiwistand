f541be5a8baa663ca790ad83f03d5e96b161a180
MacB <mac@macbudkowski.com>
2023-07-01 14:54:00 +0200

    community meta

--- src/views/community.mjs
+++ src/views/community.mjs
@@ -87,6 +87,8 @@ export default async function (trie, theme) {
     <html lang="en" op="news">
       <head>
         ${Head}
+        <title>Kiwi News Community & Leaderboard</title>
+        <meta name="description" content="Meet the Kiwi News community, which curates our feed. You can also check out our leaderboard to see who's most active.">
       </head>
       <body>
         ${Sidebar}
