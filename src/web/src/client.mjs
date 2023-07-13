010d571b168c3696af1b7581a0abe7474c7dee74
Tim Daubenschütz <tim@daubenschuetz.de>
2023-07-13 19:57:56 +0200

    Remove optimism requirement

--- src/web/src/client.mjs
+++ src/web/src/client.mjs
@@ -1,14 +1,12 @@
 // @format
 import { createClient } from "wagmi";
 import { getDefaultClient } from "connectkit";
-import { optimism } from "wagmi/chains";
 
 const client = createClient(
   getDefaultClient({
     appName: "Kiwi News",
     alchemyId: "3ZBBnBhNn0nMmNcdgXFpqWqC981hd1Z2",
     walletConnectProjectId: "cd46d2fcf6d171fb7c017129868fa211",
-    chains: [optimism],
   })
 );
 export default client;
