772e36c7de04ea06cb9ee468686934cf5909d2c0
MacB <mac@macbudkowski.com>
2023-07-13 11:16:01 +0200

    added favicon + added / in head(suggested by gpt4)

--- src/views/components/head.mjs
+++ src/views/components/head.mjs
@@ -7,8 +7,8 @@ export default html`
   <meta charset="utf-8" />
   <meta name="referrer" content="origin" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon.png" />
-  <link rel="stylesheet" type="text/css" href="news.css" />
-  <link rel="shortcut icon" href="favicon.ico" />
+  <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
+  <link rel="stylesheet" type="text/css" href="/news.css" />
+  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
   <title>Kiwi News</title>
 `;
