71899f89d53809e9d17929c70d0935b08c3e21dc
Tim Daubenschütz <tim@daubenschuetz.de>
2023-07-14 14:13:13 +0200

    Deactivate revoking delegate keys

--- src/chainstate/registry.mjs
+++ src/chainstate/registry.mjs
@@ -12,7 +12,22 @@ export async function delegations() {
   const name = database.order("list-delegations");
   const subdb = db.openDB(name);
   const all = await database.all(subdb, "");
-  const logs = all.map(({ value }) => ({ ...value, data: value.data.data }));
+  // NOTE: Since a revocation alters the set of addresses with permissions to
+  // store data on the network, and since the revocation essentially gives a
+  // user the option to remove an address from the delegation mapping
+  // indefinitely, this can cause a reconciliation bug where when a user
+  // revokes an address, the reconciliation algorithm - for synchronizations
+  // with new nodes would consider a formerly valid but now revoked signer key
+  // not part of the signer set and hence, for the new node, some received
+  // older messages would appear as invalid and split the network. A mitigation
+  // of this problem could be to delete or tombstone all messages immediately
+  // after a delegate key is revoked.
+  // Hence, below we are checking if the last bit of data[2] (`bool
+  // authorize`) is zero. If so, we're filtering it from the delegation event
+  // logs as to not allow revocations to be validated.
+  const logs = all
+    .map(({ value }) => ({ ...value, data: value.data.data }))
+    .filter(({ data }) => BigInt(data[2]) % 2n !== 0n);
   return organize(logs);
 }
 
