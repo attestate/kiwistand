92b3e9385c59e5d60fc0915010f25a12faefd37a
Tim Daubenschütz <tim@daubenschuetz.de>
2023-07-26 15:39:18 +0200

    Code splitting optimizations

--- src/web/src/main.jsx
+++ src/web/src/main.jsx
@@ -1,98 +1,8 @@
 import "vite/modulepreload-polyfill";
 import "@rainbow-me/rainbowkit/styles.css";
-import { StrictMode, useEffect, useState } from "react";
-import { createRoot } from "react-dom/client";
-import {
-  getAccount,
-  watchAccount,
-  fetchEnsAvatar,
-  fetchEnsName,
-} from "@wagmi/core";
-import { WagmiConfig } from "wagmi";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { eligible } from "@attestate/delegator2";
+import { watchAccount } from "@wagmi/core";
 
-import {
-  ConnectedProfile,
-  ConnectedLearnMore,
-  ConnectedDisconnectButton,
-  ConnectedConnectButton,
-  ConnectedSettings,
-} from "./Navigation.jsx";
-import SubmitButton from "./SubmitButton.jsx";
-import DelegateButton from "./DelegateButton.jsx";
-import Vote from "./Vote.jsx";
-import NFTPrice from "./NFTPrice.jsx";
-import OnboardingModal from "./OnboardingModal.jsx";
-import NFTModal from "./NFTModal.jsx";
 import { showMessage } from "./message.mjs";
-import { fetchAllowList, fetchDelegations } from "./API.mjs";
-import { client, chains } from "./client.mjs";
-
-function updateLink(allowlist) {
-  return (account) => {
-    const { address, isConnected } = account;
-
-    const links = document.querySelectorAll("[data-premium], [data-free]");
-    links.forEach((link) => {
-      const premiumLink = link.getAttribute("data-premium");
-      const freeLink = link.getAttribute("data-free");
-      const targetLink =
-        isConnected && allowlist.includes(address) ? premiumLink : freeLink;
-
-      link.setAttribute("href", targetLink);
-    });
-  };
-}
-
-async function start() {
-  const allowlist = await fetchAllowList();
-  const unwatch = watchAccount(updateLink(allowlist));
-
-  const delegations = await fetchDelegations();
-  addVotes(allowlist, delegations);
-  addSubmitButton(allowlist, delegations);
-}
-
-function addSubmitButton(allowlist, delegations) {
-  const submitButtonContainer = document.getElementById("submit-button");
-  if (submitButtonContainer) {
-    createRoot(submitButtonContainer).render(
-      <StrictMode>
-        <SubmitButton allowlist={allowlist} delegations={delegations} />
-      </StrictMode>
-    );
-  }
-}
-
-function addVotes(allowlist, delegations) {
-  const voteArrows = document.querySelectorAll(".votearrowcontainer");
-  if (voteArrows && voteArrows.length > 0) {
-    voteArrows.forEach((arrow) => {
-      const title = arrow.getAttribute("data-title");
-      const href = arrow.getAttribute("data-href");
-      let upvoters;
-      try {
-        upvoters = JSON.parse(arrow.getAttribute("data-upvoters"));
-      } catch (err) {
-        console.log("Couldn't parse upvoters", err);
-      }
-      createRoot(arrow).render(
-        <StrictMode>
-          <Vote
-            title={title}
-            href={href}
-            allowlist={allowlist}
-            delegations={delegations}
-            upvoters={upvoters}
-          />
-        </StrictMode>
-      );
-    });
-  }
-}
-
-start();
 
 function handleClick(event) {
   const sidebar = document.querySelector(".sidebar");
@@ -135,77 +45,185 @@ function toggleSidebar() {
 
 document.addEventListener("click", handleClick);
 
-const delegateButtonContainer = document.querySelector("delegate-button");
-if (delegateButtonContainer) {
-  createRoot(delegateButtonContainer).render(
-    <StrictMode>
-      <DelegateButton />
-    </StrictMode>
-  );
+function updateLink(allowlist) {
+  return (account) => {
+    const { address, isConnected } = account;
+
+    const links = document.querySelectorAll("[data-premium], [data-free]");
+    links.forEach((link) => {
+      const premiumLink = link.getAttribute("data-premium");
+      const freeLink = link.getAttribute("data-free");
+      const targetLink =
+        isConnected && allowlist.includes(address) ? premiumLink : freeLink;
+
+      link.setAttribute("href", targetLink);
+    });
+  };
 }
 
-const settings = document.querySelector("nav-settings");
-createRoot(settings).render(
-  <StrictMode>
-    <ConnectedSettings />
-  </StrictMode>
-);
-const profileLink = document.querySelector("nav-profile");
-createRoot(profileLink).render(
-  <StrictMode>
-    <ConnectedProfile />
-  </StrictMode>
-);
-const learnMore = document.querySelector("nav-learn-more");
-if (learnMore) {
-  createRoot(learnMore).render(
-    <StrictMode>
-      <ConnectedLearnMore />
-    </StrictMode>
-  );
-}
-const disconnect = document.querySelector("nav-disconnect");
-createRoot(disconnect).render(
-  <StrictMode>
-    <ConnectedDisconnectButton />
-  </StrictMode>
-);
-const nftmodal = document.querySelector("nav-nft-modal");
-if (nftmodal) {
-  createRoot(nftmodal).render(
-    <StrictMode>
-      <NFTModal />
-    </StrictMode>
-  );
-}
-const onboarding = document.querySelector("nav-onboarding-modal");
-if (onboarding) {
-  createRoot(onboarding).render(
-    <StrictMode>
-      <OnboardingModal />
-    </StrictMode>
-  );
-}
-
-const connectButton = document.querySelector("#connectButton");
-connectButton.style = "";
-createRoot(connectButton).render(
-  <StrictMode>
-    <ConnectedConnectButton />
-  </StrictMode>
-);
-
-const nftPriceElements = document.querySelectorAll("nft-price");
-
-if (nftPriceElements && nftPriceElements.length > 0) {
-  nftPriceElements.forEach((element) => {
-    createRoot(element).render(
+async function addSubmitButton(allowlist, delegations) {
+  const submitButtonContainer = document.getElementById("submit-button");
+  if (submitButtonContainer) {
+    const { createRoot } = await import("react-dom/client");
+    const { StrictMode } = await import("react");
+    const SubmitButton = (await import("./SubmitButton.jsx")).default;
+    createRoot(submitButtonContainer).render(
       <StrictMode>
-        <NFTPrice />
+        <SubmitButton allowlist={allowlist} delegations={delegations} />
       </StrictMode>
     );
-  });
+  }
 }
+
+async function addVotes(allowlist, delegations) {
+  const voteArrows = document.querySelectorAll(".votearrowcontainer");
+  if (voteArrows && voteArrows.length > 0) {
+    const { createRoot } = await import("react-dom/client");
+    const { StrictMode } = await import("react");
+    const Vote = (await import("./Vote.jsx")).default;
+    voteArrows.forEach((arrow) => {
+      const title = arrow.getAttribute("data-title");
+      const href = arrow.getAttribute("data-href");
+      let upvoters;
+      try {
+        upvoters = JSON.parse(arrow.getAttribute("data-upvoters"));
+      } catch (err) {
+        console.log("Couldn't parse upvoters", err);
+      }
+      createRoot(arrow).render(
+        <StrictMode>
+          <Vote
+            title={title}
+            href={href}
+            allowlist={allowlist}
+            delegations={delegations}
+            upvoters={upvoters}
+          />
+        </StrictMode>
+      );
+    });
+  }
+}
+
+async function addDelegateButton() {
+  const delegateButtonContainer = document.querySelector("delegate-button");
+  if (delegateButtonContainer) {
+    const { createRoot } = await import("react-dom/client");
+    const { StrictMode } = await import("react");
+    const DelegateButton = (await import("./DelegateButton.jsx")).default;
+    createRoot(delegateButtonContainer).render(
+      <StrictMode>
+        <DelegateButton />
+      </StrictMode>
+    );
+  }
+}
+
+async function addConnectedComponents() {
+  const { createRoot } = await import("react-dom/client");
+  const { StrictMode } = await import("react");
+  const {
+    ConnectedSettings,
+    ConnectedProfile,
+    ConnectedLearnMore,
+    ConnectedDisconnectButton,
+    ConnectedConnectButton,
+  } = await import("./Navigation.jsx");
+
+  const connectButton = document.querySelector("#connectButton");
+  connectButton.style = "";
+  createRoot(connectButton).render(
+    <StrictMode>
+      <ConnectedConnectButton />
+    </StrictMode>
+  );
+
+  const settings = document.querySelector("nav-settings");
+  createRoot(settings).render(
+    <StrictMode>
+      <ConnectedSettings />
+    </StrictMode>
+  );
+  const profileLink = document.querySelector("nav-profile");
+  createRoot(profileLink).render(
+    <StrictMode>
+      <ConnectedProfile />
+    </StrictMode>
+  );
+  const learnMore = document.querySelector("nav-learn-more");
+  if (learnMore) {
+    createRoot(learnMore).render(
+      <StrictMode>
+        <ConnectedLearnMore />
+      </StrictMode>
+    );
+  }
+  const disconnect = document.querySelector("nav-disconnect");
+  createRoot(disconnect).render(
+    <StrictMode>
+      <ConnectedDisconnectButton />
+    </StrictMode>
+  );
+}
+
+async function addModals() {
+  const nftmodal = document.querySelector("nav-nft-modal");
+  if (nftmodal) {
+    const { createRoot } = await import("react-dom/client");
+    const { StrictMode } = await import("react");
+    const NFTModal = (await import("./NFTModal.jsx")).default;
+    createRoot(nftmodal).render(
+      <StrictMode>
+        <NFTModal />
+      </StrictMode>
+    );
+  }
+
+  const onboarding = document.querySelector("nav-onboarding-modal");
+  if (onboarding) {
+    const { createRoot } = await import("react-dom/client");
+    const { StrictMode } = await import("react");
+    const OnboardingModal = (await import("./OnboardingModal.jsx")).default;
+    createRoot(onboarding).render(
+      <StrictMode>
+        <OnboardingModal />
+      </StrictMode>
+    );
+  }
+}
+
+async function addNFTPrice() {
+  const nftPriceElements = document.querySelectorAll("nft-price");
+  if (nftPriceElements && nftPriceElements.length > 0) {
+    const { createRoot } = await import("react-dom/client");
+    const { StrictMode } = await import("react");
+    const NFTPrice = (await import("./NFTPrice.jsx")).default;
+    nftPriceElements.forEach((element) => {
+      createRoot(element).render(
+        <StrictMode>
+          <NFTPrice />
+        </StrictMode>
+      );
+    });
+  }
+}
+
+async function start() {
+  await addDelegateButton();
+  await addConnectedComponents();
+  await addModals();
+  await addNFTPrice;
+
+  const { fetchAllowList, fetchDelegations } = await import("./API.mjs");
+  const allowlist = await fetchAllowList();
+  const unwatch = watchAccount(updateLink(allowlist));
+
+  const delegations = await fetchDelegations();
+  await addVotes(allowlist, delegations);
+  await addSubmitButton(allowlist, delegations);
+}
+
+start();
 
 let url = new URL(window.location.href);
 let messageParam = url.searchParams.get("message");
