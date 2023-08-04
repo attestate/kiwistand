af6bac2b783f22290659a7480b06c1e8ad1a25f2
TimDaub <TimDaub@users.noreply.github.com>
2023-08-04 09:05:18 +0000

    Prettified Code!

--- src/public/refresh-react.js
+++ src/public/refresh-react.js
@@ -1,16 +1,16 @@
 async function loadRefreshRuntime() {
-   const protocol = window.location.protocol;
-   const hostname = window.location.hostname;
-   const port = "5173"; // Default vite port
+  const protocol = window.location.protocol;
+  const hostname = window.location.hostname;
+  const port = "5173"; // Default vite port
 
-   const url = `${protocol}//${hostname}:${port}/@react-refresh`;
+  const url = `${protocol}//${hostname}:${port}/@react-refresh`;
 
-   const RefreshRuntime = await import(url);
+  const RefreshRuntime = await import(url);
 
-   RefreshRuntime.injectIntoGlobalHook(window);
-   window.$RefreshReg$ = () => {};
-   window.$RefreshSig$ = () => type => type;
-   window.__vite_plugin_react_preamble_installed__ = true;
- }
+  RefreshRuntime.injectIntoGlobalHook(window);
+  window.$RefreshReg$ = () => {};
+  window.$RefreshSig$ = () => (type) => type;
+  window.__vite_plugin_react_preamble_installed__ = true;
+}
 
- loadRefreshRuntime();
+loadRefreshRuntime();
