c62a7ec6eeb43c17524c362c4fee27ff540938c6
Tim Daubenschütz <tim@daubenschuetz.de>
2023-08-16 14:17:39 +0200

    Add preview for page

--- src/views/components/head.mjs
+++ src/views/components/head.mjs
@@ -7,8 +7,10 @@ export default html`
   <meta charset="utf-8" />
   <meta name="referrer" content="origin" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta property="og:image" content="apple-touch-icon.png" />
-  <meta property="twitter:image" content="apple-touch-icon.png" />
+  <meta property="og:image" content="preview.jpeg" />
+  <meta property="twitter:image" content="preview.jpeg" />
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="og:title" content="Kiwi News - handpicked web3 alpha">
   <link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon.png" />
   <link rel="stylesheet" type="text/css" href="news.css" />
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
