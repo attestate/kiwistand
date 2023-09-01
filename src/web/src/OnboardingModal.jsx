8227c7917c00d1c1708b0ac331009f42f2c9f1f6
Tim Daubenschütz <tim@daubenschuetz.de>
2023-09-01 15:02:47 +0200

    Detect real Safari on iOS

--- src/web/src/OnboardingModal.jsx
+++ src/web/src/OnboardingModal.jsx
@@ -5,107 +5,11 @@ import debounce from "lodash.debounce";
 
 Modal.setAppElement("nav-onboarding-modal");
 
-const PlusSVG = () => (
-  <svg
-    style={{ width: "20px", verticalAlign: "bottom" }}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 256 256"
-  >
-    <rect width="256" height="256" fill="none" />
-    <rect
-      x="40"
-      y="40"
-      width="176"
-      height="176"
-      rx="8"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="16"
-    />
-    <line
-      x1="88"
-      y1="128"
-      x2="168"
-      y2="128"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="16"
-    />
-    <line
-      x1="128"
-      y1="88"
-      x2="128"
-      y2="168"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="16"
-    />
-  </svg>
-);
-
-const ThreeDotsSVG = () => (
-  <svg
-    style={{ width: "20px", verticalAlign: "bottom" }}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 256 256"
-  >
-    <rect width="256" height="256" fill="none" />
-    <circle cx="128" cy="60" r="16" />
-    <circle cx="128" cy="128" r="16" />
-    <circle cx="128" cy="196" r="16" />
-  </svg>
-);
-
-const ShareSVG = () => (
-  <svg
-    style={{ width: "20px", verticalAlign: "bottom" }}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 256 256"
-  >
-    <rect width="256" height="256" fill="none" />
-    <path
-      d="M176,104h24a8,8,0,0,1,8,8v96a8,8,0,0,1-8,8H56a8,8,0,0,1-8-8V112a8,8,0,0,1,8-8H80"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="16"
-    />
-    <polyline
-      points="88 64 128 24 168 64"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="16"
-    />
-    <line
-      x1="128"
-      y1="24"
-      x2="128"
-      y2="136"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="16"
-    />
-  </svg>
-);
-
 export function isSafariOnIOS() {
   const ua = navigator.userAgent;
   const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
   const webkit = !!ua.match(/WebKit/i);
-  const notChrome = !ua.match(/CriOS/i);
-  const notEdge = !ua.match(/EdgiOS/i);
-  return iOS && webkit && notChrome && notEdge;
+  return iOS && webkit;
 }
 
 export function isChromeOnAndroid() {
@@ -216,6 +120,12 @@ class SimpleModal extends Component {
       };
     }
 
+    const isntReallySafari =
+      !!navigator.brave ||
+      navigator.userAgent.indexOf("CriOS") >= 0 ||
+      navigator.userAgent.match(/CriOS/i) ||
+      navigator.userAgent.match(/EdgiOS/i);
+
     return (
       <Modal
         isOpen={this.state.showModal}
@@ -276,19 +186,32 @@ class SimpleModal extends Component {
                 screen.
                 <br />
                 <br />
-                1. Tap the <ShareSVG /> Share icon
-                <br />
-                <br />
-                2. Choose "
-                <b>
-                  Add to Home Screen <PlusSVG />
-                </b>
-                "
-                <br />
-                <br />
-                3. Open the "Kiwi News" app on your home screen
-                <br />
-                <br />
+                {isntReallySafari && (
+                  <div>
+                    <b>Please switch to Safari!</b>
+                    <br />
+                    <br />
+                    On iOS <b>only</b> the Safari browser can add an app to the
+                    homescreen.
+                  </div>
+                )}
+                {!isntReallySafari && (
+                  <div>
+                    1. Tap the <ShareSVG /> Share icon
+                    <br />
+                    <br />
+                    2. Choose "
+                    <b>
+                      Add to Home Screen <PlusSVG />
+                    </b>
+                    "
+                    <br />
+                    <br />
+                    3. Open the "Kiwi News" app on your home screen
+                    <br />
+                    <br />
+                  </div>
+                )}
               </p>
             </>
           )}
@@ -315,5 +238,99 @@ class SimpleModal extends Component {
     );
   }
 }
+
+const PlusSVG = () => (
+  <svg
+    style={{ width: "20px", verticalAlign: "bottom" }}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 256 256"
+  >
+    <rect width="256" height="256" fill="none" />
+    <rect
+      x="40"
+      y="40"
+      width="176"
+      height="176"
+      rx="8"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="16"
+    />
+    <line
+      x1="88"
+      y1="128"
+      x2="168"
+      y2="128"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="16"
+    />
+    <line
+      x1="128"
+      y1="88"
+      x2="128"
+      y2="168"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="16"
+    />
+  </svg>
+);
+
+const ThreeDotsSVG = () => (
+  <svg
+    style={{ width: "20px", verticalAlign: "bottom" }}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 256 256"
+  >
+    <rect width="256" height="256" fill="none" />
+    <circle cx="128" cy="60" r="16" />
+    <circle cx="128" cy="128" r="16" />
+    <circle cx="128" cy="196" r="16" />
+  </svg>
+);
+
+const ShareSVG = () => (
+  <svg
+    style={{ width: "20px", verticalAlign: "bottom" }}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 256 256"
+  >
+    <rect width="256" height="256" fill="none" />
+    <path
+      d="M176,104h24a8,8,0,0,1,8,8v96a8,8,0,0,1-8,8H56a8,8,0,0,1-8-8V112a8,8,0,0,1,8-8H80"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="16"
+    />
+    <polyline
+      points="88 64 128 24 168 64"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="16"
+    />
+    <line
+      x1="128"
+      y1="24"
+      x2="128"
+      y2="136"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="16"
+    />
+  </svg>
+);
 
 export default SimpleModal;
