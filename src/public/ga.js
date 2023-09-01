5ddc950e5a96007299c1bc31dfeadd73677f36c8
Tim Daubenschütz <tim@daubenschuetz.de>
2023-09-01 17:28:45 +0200

    Add GA tags

--- src/public/ga.js
+++ src/public/ga.js
@@ -4,4 +4,16 @@ function gtag() {
 }
 gtag("js", new Date());
 
-gtag("config", "G-21BKTD0NKN");
+const key = "G-21BKTD0NKN";
+gtag("config", key);
+
+const isPWA = window.matchMedia("(display-mode: standalone)").matches;
+gtag("config", key, {
+  pwa_mode: isPWA ? "Standalone" : "Not Standalone",
+});
+
+const schema = /^-kiwi-news-(0x[a-fA-F0-9]{40})-key$/;
+const usesDelegation = Object.keys(localStorage).some(key => schema.test(key));
+gtag("config", key, {
+  uses_delegation: usesDelegations ? "Delegation" : "Wallet Connection",
+});
