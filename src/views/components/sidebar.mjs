33850a54b18eba63dab3b626c8491f5d2cd6da13
MacB <mac@macbudkowski.com>
2023-11-02 14:11:50 +0100

    added /welcome to sidebar

--- src/views/components/sidebar.mjs
+++ src/views/components/sidebar.mjs
@@ -68,6 +68,7 @@ const community = html`
     />
   </svg>
 `;
+
 const about = html`
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
     <rect width="256" height="256" fill="none" />
@@ -90,6 +91,20 @@ const about = html`
       stroke-width="16"
     />
     <circle cx="124" cy="84" r="12" />
+  </svg>
+`;
+
+const aboutfull = html`
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="64"
+    height="64"
+    fill="#000000"
+    viewBox="0 0 256 256"
+  >
+    <path
+      d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm-4,48a12,12,0,1,1-12,12A12,12,0,0,1,124,72Zm12,112a16,16,0,0,1-16-16V128a8,8,0,0,1,0-16,16,16,0,0,1,16,16v40a8,8,0,0,1,0,16Z"
+    ></path>
   </svg>
 `;
 
@@ -344,6 +359,18 @@ const sidebar = (path) => html`
           ${path === "/subscribe" ? envelopefull : envelope}
         </div>
         <span>Newsletter</span>
+      </div>
+    </a>
+    <a
+      title="About"
+      href="/welcome"
+      style="color: black; text-decoration: none; display: block;"
+    >
+      <div style="display: flex; align-items: center;">
+        <div class="svg-container">
+          ${path === "/welcome" ? aboutfull : about}
+        </div>
+        <span>About</span>
       </div>
     </a>
     <a
