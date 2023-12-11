43d9d5857b10eb86a6c8741f58df95c6a3eae51f
Tim Daubenschütz <tim@daubenschuetz.de>
2023-12-11 16:21:05 +0100

    tips.mjs formatting

--- src/tips.mjs
+++ src/tips.mjs
@@ -4,43 +4,43 @@ import path from "path";
 import { fetchBuilder, FileSystemCache } from "node-fetch-cache";
 
 const fetch = fetchBuilder.withCache(
-    new FileSystemCache({
-      cacheDirectory: path.resolve(env.CACHE_DIR),
-      ttl: 86400000, // 24 hours
-    }),
+  new FileSystemCache({
+    cacheDirectory: path.resolve(env.CACHE_DIR),
+    ttl: 86400000, // 24 hours
+  }),
 );
 
 const TIP_GET_ENDPOINT = "https://getusertips-zl7caqyemq-uc.a.run.app?user=";
 const TIP_API_KEY = "73yZ9m4JJccccsm0L6HNPanQm";
 
 export async function getTips(address) {
-    try {
-      // 1. Fetch tips from the API
-      const response = await fetch(`${TIP_GET_ENDPOINT}${address}`, {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${TIP_API_KEY}`
-        },
-      });
+  try {
+    // 1. Fetch tips from the API
+    const response = await fetch(`${TIP_GET_ENDPOINT}${address}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TIP_API_KEY}`,
+      },
+    });
 
-      // 2. Get the data from the response
-      const data = await response.json();
-  
-      // 3. Check if the response is valid
-      if (!data || !data.success || !data.data) {
-        return [];
-      }
-  
-      // 4. Return the formatted tips
-      return data.data.map(({ from, to, usdAmount, timestamp }) => ({
-          from,
-          to,
-          timestamp: timestamp._seconds,
-          amount: usdAmount,
-          message: `You have been tipped with $${usdAmount} USD`,
-      }));
-    } catch (error) {
-      console.error('Fetching tips failed:', error);
+    // 2. Get the data from the response
+    const data = await response.json();
+
+    // 3. Check if the response is valid
+    if (!data || !data.success || !data.data) {
       return [];
     }
+
+    // 4. Return the formatted tips
+    return data.data.map(({ from, to, usdAmount, timestamp }) => ({
+      from,
+      to,
+      timestamp: timestamp._seconds,
+      amount: usdAmount,
+      message: `You have been tipped with $${usdAmount} USD`,
+    }));
+  } catch (error) {
+    console.error("Fetching tips failed:", error);
+    return [];
   }
+}
