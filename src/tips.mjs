2b9f763847276c397aad1d48f174169148686ece
Tim Daubenschütz <tim@daubenschuetz.de>
2023-12-23 10:58:51 +0100

    Remove fetch cache from tips endpoint

--- src/tips.mjs
+++ src/tips.mjs
@@ -3,7 +3,7 @@ import path from "path";
 
 import { fetchBuilder, FileSystemCache } from "node-fetch-cache";
 
-const fetch = fetchBuilder.withCache(
+const fetchWithCache = fetchBuilder.withCache(
   new FileSystemCache({
     cacheDirectory: path.resolve(env.CACHE_DIR),
     ttl: 60, // 1 min
@@ -19,12 +19,15 @@ const TIP_API_KEY = "73yZ9m4JJccccsm0L6HNPanQm";
 export async function getUserTips(address) {
   try {
     // 1. Fetch tips from the API
-    const response = await fetch(`${USER_TIP_GET_ENDPOINT}${address}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${TIP_API_KEY}`,
+    const response = await fetchWithCache(
+      `${USER_TIP_GET_ENDPOINT}${address}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${TIP_API_KEY}`,
+        },
       },
-    });
+    );
 
     // 2. Get the data from the response
     const data = await response.json();
