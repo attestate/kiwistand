5b3adbd9958951bf790701b862170bc941faf097
Tim Daubenschütz <tim@daubenschuetz.de>
2024-01-24 16:52:40 +0100

    Integrate /community ogImage

--- src/views/community.mjs
+++ src/views/community.mjs
@@ -9,7 +9,7 @@ import * as ens from "../ens.mjs";
 import Header from "./components/header.mjs";
 import Footer from "./components/footer.mjs";
 import Sidebar from "./components/sidebar.mjs";
-import Head from "./components/head.mjs";
+import { custom } from "./components/head.mjs";
 import * as karma from "../karma.mjs";
 import * as registry from "../chainstate/registry.mjs";
 import {
@@ -91,7 +91,6 @@ async function points(identity) {
     const allowlist = Array.from(await registry.allowlist());
     rank = allowlist.length;
   }
-  const ogImage = "https://news.kiwistand.com/kiwi_community_page.png";
   return html`
     <div
       style="margin: 1rem; border: 1px solid rgba(0,0,0,0.05); border-radius: 5px; background-color: rgba(0,0,0,0.05);"
@@ -170,11 +169,12 @@ export default async function (trie, theme, query, identity) {
     search,
   );
 
+  const ogImage = "https://news.kiwistand.com/kiwi_community_page.png";
   const path = "/community";
   return html`
     <html lang="en" op="news">
       <head>
-        ${Head}
+        ${custom(ogImage)}
         <meta
           name="description"
           content="Meet the Kiwi News community, which curates our feed. You can also check out our leaderboard to see who's most active."
