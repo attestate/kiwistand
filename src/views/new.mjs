9eb032aa9489fb329e1f7ef863ac5cfa3db34fcc
Tim Daubenschütz <tim@daubenschuetz.de>
2024-01-24 16:57:55 +0100

    Integrate more custom ogImages

--- src/views/new.mjs
+++ src/views/new.mjs
@@ -13,7 +13,7 @@ import SecondHeader from "./components/secondheader.mjs";
 import ThirdHeader from "./components/thirdheader.mjs";
 import Sidebar from "./components/sidebar.mjs";
 import Footer from "./components/footer.mjs";
-import Head from "./components/head.mjs";
+import { custom } from "./components/head.mjs";
 import * as store from "../store.mjs";
 import * as moderation from "./moderation.mjs";
 import * as registry from "../chainstate/registry.mjs";
@@ -154,7 +154,7 @@ export default async function (trie, theme, index) {
   return html`
     <html lang="en" op="news">
       <head>
-        ${Head}
+        ${custom(ogImage)}
         <meta
           name="description"
           content="Explore the latest news in the decentralized world on Kiwi News. Stay updated with fresh content handpicked by crypto veterans."
