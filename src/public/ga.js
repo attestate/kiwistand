5b1407591a67ff7214d67688b80d6d771272413f
Tim Daubenschütz <tim@daubenschuetz.de>
2024-01-31 17:02:19 +0100

    Update tag manager

--- src/public/ga.js
+++ src/public/ga.js
@@ -8,14 +8,12 @@ const key = "G-21BKTD0NKN";
 gtag("config", key);
 
 const isPWA = window.matchMedia("(display-mode: standalone)").matches;
-gtag("config", key, {
-  pwa_mode: isPWA ? "Standalone" : "Not Standalone",
-});
-
 const schema = /^-kiwi-news-(0x[a-fA-F0-9]{40})-key$/;
 const usesDelegation = Object.keys(localStorage).some((key) =>
   schema.test(key),
 );
+
 gtag("config", key, {
+  pwa_mode: isPWA ? "Standalone" : "Not Standalone",
   uses_delegation: usesDelegation ? "Delegation" : "Wallet Connection",
 });
