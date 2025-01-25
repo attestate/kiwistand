8eb2d545b6f31034bdfa45cdedfb8ea18cae1b16
Tim Daubenschütz <tim@daubenschuetz.de>
2025-01-25 18:33:01 +0100

    reset curation module

--- src/views/curation.mjs
+++ src/views/curation.mjs
@@ -6,12 +6,20 @@ import { fetchBuilder, FileSystemCache } from "node-fetch-cache";
 
 import { fetchCache } from "../utils.mjs";
 
+const minuteInMs = 60000;
+const cache = new FileSystemCache({
+  cacheDirectory: path.resolve(env.CACHE_DIR),
+  ttl: minuteInMs,
+});
+const fetch = fetchBuilder.withCache(cache);
+const fetchStaleWhileRevalidate = fetchCache(fetch, cache);
+
 const url =
   "https://opensheet.elk.sh/1R9zOdQPNo-UZqBNMvvnLig7eabKkTfMLjs1h_RGflaM/";
 
 export async function getSheet(sheetName) {
   const signal = AbortSignal.timeout(5000);
-  const response = await fetch(url + sheetName, { signal });
+  const response = await fetchStaleWhileRevalidate(url + sheetName, { signal });
   if (!response.ok) {
     throw new Error("Couldn't fetch the sheet");
   }
