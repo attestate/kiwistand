5dd3de27b12f4d4bf20617732e9cc5f94972b5e2
Tim Daubenschütz <tim@daubenschuetz.de>
2025-02-26 15:48:00 +0100

    refactor: Remove onboarding link from footer

--- src/views/components/footer.mjs
+++ src/views/components/footer.mjs
@@ -56,7 +56,6 @@ const footer = (theme, path) => html`
       <div>
         <strong>Resources</strong><br />
         <a href="/privacy-policy">Privacy Policy</a><br />
-        <a href="/onboarding-reader">Onboarding</a><br />
         <a href="/shortcut">iOS Shortcut</a><br />
       </div>
       <div>
