56159da26a63f56cedc9f5baf33161e801ce1fb1
Tim Daubenschütz <tim@daubenschuetz.de>
2025-03-01 15:05:55 +0100

    perf: Optimize script loading and preconnect performance

--- src/views/components/head.mjs
+++ src/views/components/head.mjs
@@ -26,6 +26,7 @@ export function custom(
   return html`
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://www.googletagmanager.com">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" media="print" onload="this.media='all'">
     <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"></noscript>
     <script src="event-queue.js"></script>
@@ -45,7 +46,8 @@ export function custom(
     ${ogDescription
       ? html`<meta property="og:description" content="${ogDescription}" />`
       : ""}
-    <link rel="stylesheet" type="text/css" href="news.css" />
+    <link rel="preload" href="news.css" as="style">
+    <link rel="stylesheet" href="news.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     ${PwaLinks()}
     <title>${ogTitle}</title>
