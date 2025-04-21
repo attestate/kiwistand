5cc3d9a6ba9f51d58dbdbcd89db95129cfb529b1
Tim Daubenschütz <tim@daubenschuetz.de>
2025-04-21 12:14:42 +0200

    chore: Remove Ethereum-only submission notice

--- src/views/submit.mjs
+++ src/views/submit.mjs
@@ -136,7 +136,6 @@ export default async function submit(theme, url = "", title = "") {
                       >
                         Loading...
                       </button>
-                      <p>Please <b>only</b> submit Ethereum-related content</p>
                     </div>
                   </form>
                 </td>
